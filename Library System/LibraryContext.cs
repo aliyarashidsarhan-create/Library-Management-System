96ba780e0f53143ca038e71f4f4261a400c75f28
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-23 10:20:33 +0400

    create LibraryContext

---
+++ Library System/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using Library_System.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System
+{
+    public  class LibraryContext
+    {
+        public List<Book> Books {  get; set; }
+        public List<Member> members { get; set; }
+        public List<Loan> loans { get; set; }
+        public List<Reservation> reservations { get; set; }
+    }
+}
--- Library System/Module/Loan.cs
+++ Library System/Module/Loan.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace Library_System.Module
 {
-    public class Loans
+    public class Loan
     {
         public int loanId {  get; set; }//system generated
         public int bookId { get; set; }//user input from List of book
         public int memberId { get; set; }//user input from List of member
-        public string borrowDate { get; set; }//
-        public string dueDate { get; set; }
-        public string returnDate { get; set; }
-        public decimal fineAmount {  get; set; }
+        public string borrowDate { get; set; }//System Generated
+        public string dueDate { get; set; }//calculate
+        public string returnDate { get; set; }//user input
+        public decimal fineAmount {  get; set; }//calculate
 
     }
 }
